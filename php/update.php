7750e9b1a9010ad40b2ad43aec2637d4ebe5a088
jlm <jean-luc.metzger@univ-lorraine.fr>
2013-12-04 16:11:17 +0100

    Ce code php récupère bien les valeurs transmises par l’application ExtJS. Reste à faire l’update sur les valeurs choisie

--- php/update.php
+++ php/update.php
@@ -1,4 +1,26 @@
 <?php
-  print_r($_REQUEST);
-  echo 'toto'
+  $nbEntree = 0;
+  foreach($_REQUEST as $key => $value) {
+    $arr[$key]= $value;
+    $nbEntree++;
+  }
+  $decodeRecord = json_decode(stripslashes($_REQUEST['records']));
+  $arr['decode']=$decodeRecord;
+  send($arr, $nbEntree);
+
+  function send($output, $nbrows) {
+    $callback = $_REQUEST['callback'];
+    $json = '""';
+    if($nbrows>0) {
+      $json = json_encode($output);
+    }
+    if ($callback) {
+        header('Content-Type: text/javascript');
+        echo $callback . '(' .'{"total":"'.$nbrows.'","results":'. $json .'}' . ');';
+    } else {
+        header('Content-Type: application/x-json');
+        echo '{"total":"'.$nbrows.'","results":'. $json .'}';
+    }
+  }
+
 ?>
